8b56e108169957de4fb0ac1364b0d06e4ade956e
ByteSculptor18 <sayanbiswascoc0026@gmail.com>
2026-04-26 20:00:55 +0530

    basic

--- Scripts/To-Do-List.js
+++ Scripts/To-Do-List.js
@@ -1,32 +1,51 @@
 const arr = [];
-function addElement(){
-    const element = document.querySelector(".js-input");
-    const val = element.value;
-    if (!val) return;
-    arr.push(val);
-    console.log(arr);
-    element.value = '';
-    Render();
 
+function addElement(){
+    const inputEl = document.querySelector(".js-input");
+    const dateEl = document.querySelector(".js-date");
+
+    const name = inputEl.value;
+    const date = dateEl.value;
+
+    if (!name) return;
+    if (!date) return;
+
+    arr.push({
+        name: name,
+        date: date
+    });
+
+    inputEl.value = '';
+    dateEl.value = '';
+
+    Render();
 }
+
 function Render(){
     const val = document.querySelector(".js-tasks");
+
     let print = "";
+
     for(let i = 0; i < arr.length; i++){
-        print += `<p>
-        ${i + 1}. ${arr[i]} <button onclick="
-        removeElement(${i})">Remove</button>
+        const todoObject = arr[i];
+
+        print += `
+        <p>
+            ${i + 1}. ${todoObject.name} (${todoObject.date || "No date"})
+            <button onclick="removeElement(${i})">Remove</button>
         </p>`;
     }
+
     val.innerHTML = print;
 }
+
 function removeElement(index){
     arr.splice(index, 1);
     Render();
 }
+
 function Type(value) {
-            if (value === "Enter") {
-                addElement();
-                return "Enter Key";
-            }
+    if (value === "Enter") {
+        addElement();
+    }
 }
